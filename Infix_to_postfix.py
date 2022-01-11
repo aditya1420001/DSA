78bde7eb3f11f368ff95932283b4f313a22dbf68
aditya1420001 <97335393+aditya1420001@users.noreply.github.com>
2022-01-12 00:04:19 +0530

    Update Infix_to_postfix.py

--- Infix_to_postfix.py
+++ Infix_to_postfix.py
@@ -1,2 +1,45 @@
 print("Hello world")
-print("")
+print("")class Solution:
+    def precedence(self, x):
+        if x == "^":
+            return 3
+        elif x in ["*", "/"]:
+            return 2
+        elif x in ["+", "-"]:
+            return 1
+        elif x in ["(", ")"]:
+            return -1
+        else:
+            return 0
+    # Function to convert an infix expression to a postfix expression.
+
+    def InfixtoPostfix(self, exp):
+        arr = []
+        ans = ""
+        for ele in exp:
+            val = self.precedence(ele)
+            if val == 0:
+                ans += ele
+                continue
+            else:
+                if ele == "(":
+                    arr.append(ele)
+                elif ele == ")":
+                    while(len(arr) != 0 and arr[-1] != "("):
+                        var = arr.pop()
+                        ans += var
+                    if len(arr) != 0 and arr[-1] == "(":
+                        arr.pop()
+                else:
+                    while(len(arr) != 0 and self.precedence(arr[-1]) >= val):
+                        temp = arr.pop()
+                        ans += temp
+                    arr.append(ele)
+        while(len(arr) != 0):
+            temp = arr.pop()
+            ans += temp
+        return ans
+
+a=Solution()
+res=a.InfixtoPostfix("a+b*(c^d-e)^(f+g*h)-i")
+print(res)
